8676a7eabe80cf97e7c2a4d1a32df36a44b804b5
Juarez Bochi <jbochi@gmail.com>
2017-01-18 11:00:55 -0200

    Add config and more tests

--- foo/foo.go
+++ foo/foo.go
@@ -1,5 +1,33 @@
 package foo
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
 func addsTwo(x int) int {
-  return x + 2;
+	return x + 2
+}
+
+func getDataFromJson(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	type Article struct {
+		ID   int
+		Name string
+	}
+	var articles []Article
+	err = json.Unmarshal(body, &articles)
+	if err != nil {
+		return "", err
+	}
+	return articles[0].Name, nil
 }
--- foo/foo_test.go
+++ foo/foo_test.go
@@ -1,9 +1,29 @@
 package foo
 
-import "testing"
+import (
+	"gopkg.in/jarcoal/httpmock.v1"
+	"testing"
+)
 
 func TestSomething(t *testing.T) {
-  if addsTwo(10) != 12 {
-      t.Errorf("Can't sum!")
-  }
+	if addsTwo(10) != 12 {
+		t.Errorf("Can't sum!")
+	}
+}
+
+func TestMock(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	url := "https://api.mybiz.com/articles.json"
+	httpmock.RegisterResponder("GET", url,
+		httpmock.NewStringResponder(200, `[{"id": 1, "name": "My Great Article"}]`))
+
+	article, err := getDataFromJson(url)
+	if err != nil {
+		t.Error(err)
+	}
+	if article != "My Great Article" {
+		t.Errorf("Wrong article!")
+	}
 }
